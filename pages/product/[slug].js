8c840a63f79402cee99b54baa5401eba7ac710b2
an196 <an9x0010@gmail.com>
2022-09-13 21:48:06 +0700

    modify detail of product

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product, Layout } from '~/components';
 import { useStateContext } from '~/context/StateContext';
+import ReactHtmlParser from 'react-html-parser';
 import { useGetProductQuery } from '~/features/product/productApiSlice';
 import Image from 'next/image';
 import fallbackImage from '~/assets/default-image.png';
@@ -11,7 +12,7 @@ function ProductDetails({ product }) {
 	const [index, setIndex] = useState(0);
 
 	const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
-	
+
 	const handleBuyNow = () => {
 		onAdd(product, qty);
 		setShowCart(true);
@@ -21,13 +22,11 @@ function ProductDetails({ product }) {
 		<div>
 			<div className='flex gap-10 m-10 mt-14 text-[#324d67]  font-semibold hlg:flex-wrap md:gap-6 sm:m-8 ssm:m-4'>
 				<div>
-					<div className='rounded-2xl bg-[#ebebeb] w-[360px] h-[360px] cursor-pointer duration-300 ease-in-out hover:bg-[#f02d34] md:w-[300px] md:h-[300px] 
+					<div
+						className='rounded-2xl bg-[#ebebeb] w-[360px] h-[360px] cursor-pointer duration-300 ease-in-out hover:bg-[#f02d34] md:w-[300px] md:h-[300px] 
 					relative sm:w-[250px] sm:h-[250px] ssm:w-[180px] ssm:h-[180px] '
 					>
-						<Image
-							src={(product?.image && product?.image[index]) || fallbackImage}
-							layout='fill'
-						/>
+						<Image src={(product?.image && product?.image[index]) || fallbackImage} layout='fill' />
 					</div>
 					<div className='flex gap-2 mt-5 sm:flex-wrap'>
 						{product?.image?.map((item, i) => (
@@ -58,17 +57,23 @@ function ProductDetails({ product }) {
 
 					<h4 className='mt-5 text-[24px] sm:text-[20px] '>Details:</h4>
 					<p className='mt-5 text-[20px] sm:text-[16px] sm:flex sm:flex-wrap text-justify sm:w-full'>
-						{product?.details}
+						{ReactHtmlParser(product?.details)}
 					</p>
 					<p className='font-bold text-2xl mt-7 text-[#f02d34] sm:text-xl'>${product?.price}</p>
 					<div className='flex gap-5 mt-5 items-center hsm:gap-2 hsm:flex-wrap'>
 						<h3 className='text-[20px]'>Quantity: </h3>
 						<p className='border-1 border-gray-700 flex mt-1 rounded-xl'>
-							<span className='text-base py-1 px-3 border-r-1 border-gray-700 text-[#f02d34] cursor-pointer hover:bg-[#f02d34]/10 rounded-l-sm' onClick={decQty}>
+							<span
+								className='text-base py-1 px-3 border-r-1 border-gray-700 text-[#f02d34] cursor-pointer hover:bg-[#f02d34]/10 rounded-l-sm'
+								onClick={decQty}
+							>
 								<AiOutlineMinus />
 							</span>
 							<span className='py-1 px-3 border-r-1 border-gray-700 text-xs'>{qty}</span>
-							<span className='text-base py-1 px-3 plus text-[#31a831]  cursor-pointer hover:bg-[#31a831]/10 rounded-r-sm' onClick={incQty}>
+							<span
+								className='text-base py-1 px-3 plus text-[#31a831]  cursor-pointer hover:bg-[#31a831]/10 rounded-r-sm'
+								onClick={incQty}
+							>
 								<AiOutlinePlus />
 							</span>
 						</p>
