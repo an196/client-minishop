11b15fcc38ff84d058b0552ef30d13fd9783a094
an196 <an9x0010@gmail.com>
2022-09-21 21:33:11 +0700

    update code

--- components/OrtherCategory.jsx
+++ components/OrtherCategory.jsx
@@ -12,11 +12,11 @@ function OrtherCategory() {
 	return (
 		<div className=''>
             <HeadTitile title={'Category'} subtitle={'Speakers of many variations'}/>
-			<div className='flex gap-4 hlg:grid hlg:grid-cols-2 hlg:place-items-center sm:grid-cols-1'>
+			<div className='flex gap-4 hlg:grid hlg:grid-cols-2 hlg:place-items-center sm:gap-1 m-1 ssm:grid-cols-1'>
 				{images.map((image) => (
-					<div className='relative group w-[25%] h-[340px] xl:h-[260px] lg:h-[240px] md:h-[240px]  hlg:w-[240px]  rounded-md overflow-hidden'>
+					<div className='relative group w-[25%] h-[340px] xl:h-[260px] lg:h-[240px] md:h-[240px] hlg:w-full hlg:h-[32vh] sm:h-[22vh]  rounded-md overflow-hidden'>
 						<Image src={image} alt='dsd' layout='fill' objectFit='center'  />
-						<div className='absolute w-full h-[340px] xl:h-[260px] lg:h-[240px]  bg-[#3d3d3d]/60 !z-10 '></div>
+						<div className='absolute w-full h-[340px] xl:h-[260px] lg:h-[240px] hlg:h-[32vh] sm:h-[22vh] bg-[#3d3d3d]/60 !z-10 '></div>
 						<div className='absolute flex text-center justify-center items-center w-[100%] h-[100%] bg-white/10 !z-20 opacity-0 hover:opacity-100  '>
 							<button className='py-2 px-4 bg-white rounded-full flex  items-center space-x-1 hover:scale-105 !z-30'>
 								<div className='rounded-full text-black'> View</div>
--- components/RowItem.jsx
+++ components/RowItem.jsx
@@ -6,7 +6,7 @@ function RowItem({title,subtitle,products }) {
 		<div>
 			<HeadTitile title={'Beset selling headphone'} subtitle={'Speakers of many variations'} />
 			<div className='grid grid-cols-5 gap-[15px] mt-[20px] w-full px-20 md:px-14 sm:px-[18px] xl:grid-cols-4 lg:px-0 lg:gap-0 
-				place-items-center ssm:flex-nowrap ssm:flex-col hlg:grid-cols-2 sm:grid-cols-1'>
+				place-items-center ssm:flex-nowrap ssm:flex-col hlg:grid-cols-2 ssm:grid-cols-1'>
 				{products?.slice(0, 5).map((product) => (
 					<Product key={product._id} product={product} />
 				))}
