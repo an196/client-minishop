5b766804d0f99780c6a09fecdcdd94baf82eb405
an196 <an9x0010@gmail.com>
2022-10-22 12:15:56 +0700

    udapte config

--- next.config.js
+++ next.config.js
@@ -2,18 +2,18 @@
 
 const nextConfig = {
 	reactStrictMode: true,
-	// extends: ['next/core-web-vitals', 'prettier'],
-	// ignorePatterns: ['node_modules', 'dist'],
-	// parserOptions: {
-	// 	babelOptions: {
-	// 	  presets: [require.resolve('next/babel')],
-	// 	},
-	// },
-	// eslint: {
-	// 	// Warning: This allows production builds to successfully complete even if
-	// 	// your project has ESLint errors.
-	// 	ignoreDuringBuilds: true,
-	//   },
+	extends: ['next/core-web-vitals', 'prettier'],
+	ignorePatterns: ['node_modules', 'dist'],
+	parserOptions: {
+		babelOptions: {
+			presets: [require.resolve('next/babel')],
+		},
+	},
+	eslint: {
+		// Warning: This allows production builds to successfully complete even if
+		// your project has ESLint errors.
+		ignoreDuringBuilds: true,
+	},
 	// webpack: (config) => {
 	// 	config.plugins = config.plugins || [];
 
@@ -23,12 +23,13 @@ const nextConfig = {
 	// 		...config.resolve.alias,
 	// 		'~': path.resolve(__dirname, './'),
 	// 	};
-	
+
 	// 	return config;
 	// },
 	images: {
 		domains: ['firebasestorage.googleapis.com'],
 	},
-};
+}
+
 
 module.exports = nextConfig;
