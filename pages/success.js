31f9e86204e04c56a463be46c868d011e6a1c880
an196 <46616063+an196@users.noreply.github.com>
2022-11-25 20:23:10 +0700

    Update success.js

--- pages/success.js
+++ pages/success.js
@@ -7,7 +7,6 @@ import Image from 'next/image';
 import { selectCurrentUser } from '../features/auth/authSlice';
 import { usePostOrderMutation } from '../features/order/orderApiSlice';
 import { useSelector  } from 'react-redux';
-import requiredAuth from '../features/auth/requiredAuth';
 
 function Success() {
 	const userInfo = useSelector(selectCurrentUser);
